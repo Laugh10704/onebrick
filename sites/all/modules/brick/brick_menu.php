183c7c47741c3f2d46740f5d0a292e91573b9151
Jordan Laughlin <laughy@gmail.com>
2014-02-04 14:49:15 -0800

    fix exception when you click on "chapters"

--- sites/all/modules/brick/brick_menu.php
+++ sites/all/modules/brick/brick_menu.php
@@ -101,7 +101,7 @@ function brick_menu() {
 
   $items['chapters'] = array(
      'title' => 'Chapters',
-     'page callback' => 'brick_switch_chapter',
+     'page callback' => 'brick_stay_on_same_page',
      'access callback' => 'brick_access',
      'weight' => -50,
      'menu_name' => 'main-menu',
