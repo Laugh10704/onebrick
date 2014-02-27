56539878d7816e6ff6611fba51d87268b76324f6
Benson <bensonl@google.com>
2014-02-26 22:30:32 -0800

    .hide last name in user profile

--- sites/all/themes/onebrick_tarski/user-profile.tpl.php
+++ sites/all/themes/onebrick_tarski/user-profile.tpl.php
@@ -1,14 +1,15 @@
 <div class="profile"<?php print $attributes; ?>>
 
 <?php 
-	echo "<div id='profile_title'>".$field_user_fullname[0]['safe_value']."</div>";
+	$username = $user_profile['field_user_profile_q1']['#object']->field_user_fullname['und'][0]['safe_value'];
+	echo "<div id='profile_title'>asdfa".brick_format_name($username)."</div>";
 
   print render($user_profile);
 ?>
 
 <?php
 	print("<dev>");
-	//print_r ($user_profile);
+//	print_r ($user_profile);
 	print("</dev>");
 ?>
 
