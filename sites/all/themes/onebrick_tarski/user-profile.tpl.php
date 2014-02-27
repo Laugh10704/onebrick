08c842d4956eb1a2b9a05bb91cafcd079f7c29ae
Benson <bensonl@google.com>
2014-02-26 22:32:18 -0800

    removed test string when hide last name in user profile

--- sites/all/themes/onebrick_tarski/user-profile.tpl.php
+++ sites/all/themes/onebrick_tarski/user-profile.tpl.php
@@ -2,7 +2,7 @@
 
 <?php 
 	$username = $user_profile['field_user_profile_q1']['#object']->field_user_fullname['und'][0]['safe_value'];
-	echo "<div id='profile_title'>asdfa".brick_format_name($username)."</div>";
+	echo "<div id='profile_title'>".brick_format_name($username)."</div>";
 
   print render($user_profile);
 ?>
