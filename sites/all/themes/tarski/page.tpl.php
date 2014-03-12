af0a7d668906c0e37c822e45763d4421cfb01597
Jordan Laughlin <laughy@gmail.com>
2014-03-12 14:29:24 -0700

    nevermind don't center

--- sites/all/themes/tarski/page.tpl.php
+++ sites/all/themes/tarski/page.tpl.php
@@ -90,7 +90,7 @@
             $detect = mobile_detect_get_object();
             $is_mobile = $detect->isMobile();
             if ($is_mobile) {
-                 echo "jQuery('#loginLink').colorbox({inline: true, href: '#currentPopupForm', transition: 'none', width: '400', fixed: true, top: 0, title: 'Login', opacity: '0.50', reposition: false});";
+                 echo "jQuery('#loginLink').colorbox({inline: true, href: '#currentPopupForm', transition: 'none', width: '400', fixed: true, left: 0, top: 0, title: 'Login', opacity: '0.50', reposition: false});";
                 echo "jQuery('#signupLink').colorbox({inline: true, href: '#currentPopupForm', transition: 'none', width: '400', fixed: true, left: 0, top: 0, title: 'Sign Up', opacity: '0.50', reposition: false});";
                echo "jQuery('#newUserLink').colorbox({inline: true, href: '#currentPopupForm', transition: 'elastic', width: '400', fixed: true, left: 0, top: 0, title: 'Sign Up', opacity: '0.50', reposition: false});";
                 echo "addVolunteerMenuItem.colorbox({inline: true, href: '#currentPopupForm', transition: 'none', width: '400', fixed: true, left: 0, top: 0, title: 'Add Volunteer', opacity: '0.50', reposition: false});";
